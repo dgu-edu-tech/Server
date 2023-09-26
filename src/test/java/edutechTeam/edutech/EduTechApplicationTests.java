2178a4d56a3285d17997182c6221e5eba33ba9c7
유수민 <proysm5154@naver.com>
2023-09-26 10:20:36 +0900

    update : edu-tech-Server 코드 업로드

---
+++ src/test/java/edutechTeam/edutech/EduTechApplicationTests.java
@@ -0,0 +1,13 @@
+package edutechTeam.edutech;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EduTechApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
